092c03eb534608d6524e2bd570956a8e79f87fb9
Anwesh43 <anweshthecool0@gmail.com>
2018-01-07 10:58:28 +0530

    adding State

--- radianrectview/src/main/java/ui/anwesome/com/radianrectview/RadianRectView.kt
+++ radianrectview/src/main/java/ui/anwesome/com/radianrectview/RadianRectView.kt
@@ -33,6 +33,22 @@ class RadianRectView(ctx:Context):View(ctx) {
 
         }
     }
+    data class State(var scale:Float = 0f,var dir:Float = 0f,var prevScale:Float = 0f) {
+        fun update(stopcb:(Float)->Unit) {
+            scale += 0.1f*dir
+            if(Math.abs(scale - prevScale) > 1) {
+                scale = prevScale+dir
+                dir = 0f
+                prevScale = scale
+                stopcb(scale)
+            }
+        }
+        fun startUpdating(startcb:()->Unit) {
+            if(dir == 0f) {
+                dir = 1-2*scale
+            }
+        }
+    }
 }
 fun Canvas.drawRect(x:Float,y:Float,w:Float,h:Float,start:Float,sweep:Float,paint:Paint) {
     save()
