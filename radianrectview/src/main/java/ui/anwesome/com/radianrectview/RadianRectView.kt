5c78be3cd926425f22eaf1d80d957a7459df9673
Anwesh43 <anweshthecool0@gmail.com>
2018-01-07 10:35:48 +0530

    adding RadianRectView

--- radianrectview/src/main/java/ui/anwesome/com/radianrectview/RadianRectView.kt
+++ radianrectview/src/main/java/ui/anwesome/com/radianrectview/RadianRectView.kt
@@ -1,5 +1,22 @@
 package ui.anwesome.com.radianrectview
 
+import android.view.*
+import android.content.*
+import android.graphics.*
 /**
  * Created by anweshmishra on 07/01/18.
  */
+class RadianRectView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
