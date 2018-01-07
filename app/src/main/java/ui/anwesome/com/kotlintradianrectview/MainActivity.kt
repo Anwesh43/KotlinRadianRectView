227f5fd90815348ea071c6335950db8c688e10f1
Anwesh43 <anweshthecool0@gmail.com>
2018-01-07 19:43:12 +0530

    adding RadianRectView in app

--- app/src/main/java/ui/anwesome/com/kotlintradianrectview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlintradianrectview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlintradianrectview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.radianrectview.RadianRectView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RadianRectView.create(this)
     }
 }
